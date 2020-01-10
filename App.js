80e03c59918b95edb588b38c1f6dc8cf06cdf136
AndreFelipeGomes <andrefelipe_bg@outlook.com>
2020-01-10 14:32:08 -0300

    statusBar

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Routes from "./src/pages/main";
+import "./src/config/statusBarConfig"
+
+const App = () => <Routes/>;
+
+export default App;
---
+++ src/pages/main.js
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import {Text, View} from "react-native";
+
+export default class Main extends Component {
+    static navigatorOptions = {
+        title: "Salve os Dog"
+    };
+     render(){
+         return(
+         <View>
+             <Text>Pagina Principal</Text>
+         </View>
+         )};
+}
