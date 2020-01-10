177f7ed735bf0d26b323bb24e35c87e2f6796304
AndreFelipeGomes <andrefelipe_bg@outlook.com>
2020-01-10 14:53:50 -0300

    api

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,10 +1,23 @@
 import React, {Component} from "react";
 import {Text, View} from "react-native";
+import api from "../services/api"
 
 export default class Main extends Component {
     static navigatorOptions = {
         title: "Salve os Dog"
     };
+
+    componentDidMount(){
+        this.loadProducts();
+    }
+
+    loadProducts = async () => {
+        const response = await api.get("/products");
+        const { docs } = response.data;
+
+        console.log(docs);
+    }
+
      render(){
          return(
          <View>
